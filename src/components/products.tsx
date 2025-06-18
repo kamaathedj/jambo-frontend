73fe7be3704c56ae284b0a39b5d13bc31d94224e
kamaathedj <kamaudavikiruku@gmail.com>
2025-06-18 14:16:28 +0300

    Added bulk sms page

--- src/components/products.tsx
+++ src/components/products.tsx
@@ -30,6 +30,14 @@ function Products(){
               Action 3
             </button>
           </div>
+          {/* card 4 */}
+          <div className="bg-white shadow-lg rounded-lg p-6 w-72">
+            <h2 className="text-xl font-semibold text-gray-800 mb-2">Jambo sms</h2>
+            <p className="text-gray-600 mb-4">Bulk sms at affordable.</p>
+            <button className="bg-amber-600 text-white px-4 py-2 rounded hover:bg-amber-700 transition">
+              Action 3
+            </button>
+          </div>
   
         </div>
       </div>

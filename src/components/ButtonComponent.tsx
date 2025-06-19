02f25ad5d8d26514e6bfa4ef29998c7e3bef23b4
kamaathedj <kamaudavikiruku@gmail.com>
2025-06-19 15:22:38 +0300

    :fire: custom button for the site

---
+++ src/components/ButtonComponent.tsx
@@ -0,0 +1,14 @@
+
+import React from 'react'
+
+type Props = {
+    title:string
+}
+
+export default function ButtonComponent({title}: Props) {
+  return (
+    <button className="bg-primary-500 hover:bg-primary-600 text-white px-5 py-2 rounded-lg shadow">
+    {title}
+  </button>
+  )
+}

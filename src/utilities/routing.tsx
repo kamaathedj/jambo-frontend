de176df421d3d8f311360b72bdd19e7e7f9c2af1
kamaathedj <kamaudavikiruku@gmail.com>
2025-06-19 12:16:22 +0300

    routing for not found and service client routing

--- src/utilities/routing.tsx
+++ src/utilities/routing.tsx
@@ -1,10 +1,17 @@
 import{createHashRouter} from 'react-router-dom'
 
 import App from '../App.tsx';
+import ServicesPage from '../pages/ServicesPage.tsx';
+import NotFound from '../pages/NotFound.tsx';
 
 const router = createHashRouter([{
   path:'/',
-  element: <App/>
-}]);
+  element: <App/>,
+  errorElement:<NotFound/>
+},{
+  path:'/service',
+  element:<ServicesPage/>
+}
+]);
 
 export default router;

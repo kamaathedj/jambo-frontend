85462c41def4e7b6a068e8d6c7c488d8ec9a4a36
kamaathedj <kamaudavikiruku@gmail.com>
2025-06-23 12:00:15 +0300

    add to sevices page

--- src/pages/ServicesPage.tsx
+++ src/pages/ServicesPage.tsx
@@ -1,12 +1,14 @@
 
 import Header from "../components/header";
 import RightComponent from "../components/RightComponent";
+import LeftComponent from "../components/LeftComponent";
 export default function ServicesPage() {
   return (
     <>
         <Header/>  
 
         <RightComponent/>
+        <LeftComponent/>
     </>
   )
 }
